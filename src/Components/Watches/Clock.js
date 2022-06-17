2bb8cef0e36008296468f67e8f899e8ba90ee13c
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-06-17 10:15:01 +0300

    commit 17.06

--- src/Components/Watches/Clock.js
+++ src/Components/Watches/Clock.js
@@ -1,16 +1,17 @@
 import { useEffect, useRef, useState } from "react"
 import { DateTime } from "luxon"
 
-const Clock = ({name,offset,time,sec})=>{
-    const offsetTime = time.plus({seconds:sec,hours:offset})
+const Clock = ({name,offset,time,sec,handleRemove})=>{
+    const offsetTime = time.plus({seconds:sec,minutes:new Date().getTimezoneOffset(),hours:offset})
     return(
         <div>
             <div>{name}</div>
             <div>{offsetTime.c.hour}:{offsetTime.c.minute>9?offsetTime.c.minute:'0'+offsetTime.c.minute}:{offsetTime.c.second>9?offsetTime.c.second:'0'+offsetTime.c.second}</div>
+            <span onClick={handleRemove}>X</span>
         </div>
     )
 }
-const ClockList = ({watches})=>{
+const ClockList = ({watches,removeWatch})=>{
     const [time, setTime] = useState(DateTime.now())
     const [sec, setSec] = useState(0)
     const timerId = useRef(null)
@@ -25,8 +26,8 @@ const ClockList = ({watches})=>{
     console.log(sec)
     return(
         <div>
-            {watches.map(watch=>{
-                return <Clock key={watch.name+watch.zone} name={watch.name} offset={watch.zone} time={time} sec={sec}/>
+            {watches.map((watch,key)=>{
+                return <Clock key={watch.name+watch.zone} name={watch.name} offset={watch.zone} time={time} sec={sec} handleRemove={()=>removeWatch(key)}/>
             })}
         </div>
     )
--- src/Components/Watches/index.js
+++ src/Components/Watches/index.js
@@ -4,18 +4,21 @@ const initialForm = {name:'', zone: 0}
 const Watches = () => {
     const [form, setForm] = useState(initialForm)
     const [watches, setWatches] = useState([])
-    const addWatches = useCallback(()=>{
+    const addWatch = useCallback(()=>{
         setWatches(prev=>[...prev,form])
         setForm(initialForm)
     },[form])
+    const removeWatch = useCallback((index)=>{
+       setWatches(prev=>prev.filter((f,key)=>key!==index))
+    },[])
     return(
         <div>
             <form>
                 <input type='text' value={form.name} onChange={(e)=>setForm(prev=>({...prev,name:e.target.value}))}/>
                 <input type='number' value={form.zone} onChange={(e)=>setForm(prev=>({...prev,zone:parseInt(e.target.value)}))}/>
-                <button type='button' onClick={addWatches}>Добавить</button>
+                <button type='button' onClick={addWatch}>Добавить</button>
             </form>
-            <ClockList watches={watches}/>
+            {watches.length ? <ClockList watches={watches} removeWatch={removeWatch}/> : ''}
         </div>
     )
 }
